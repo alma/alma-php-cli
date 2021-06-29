e93991b004bf62069bc4f57c0b2a52f7a1588c1c
Sylvain Just <sy.just@i-sloth.com>
2021-06-29 14:36:26 +0200

    feat: format output with table (remove dump output)

--- src/Command/AlmaMerchantGetCommand.php
+++ src/Command/AlmaMerchantGetCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use Alma\API\RequestError;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class AlmaMerchantGetCommand extends AbstractAlmaCommand
 {
@@ -20,8 +21,37 @@ class AlmaMerchantGetCommand extends AbstractAlmaCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        dump( $this->alma->merchants->me() );
+        $io = new SymfonyStyle($input, $output);
+        $merchant = $this->alma->merchants->me();
+        $headers = ['Id', 'Name', 'Website', 'Plans'];
+        $rows[] = [
+            $merchant->id,
+            $merchant->name,
+            $merchant->website,
+            implode("\n", $this->formatFeePlans($merchant->fee_plans)),
+        ];
+        $io->table($headers, $rows);
 
         return self::SUCCESS;
+    }
+
+    private function formatFeePlans(array $feePlans): array
+    {
+        $plans = [];
+        foreach ($feePlans as $feePlan) {
+            $plans[] = sprintf(
+                "cnt:'%sx', allowed:'%s',\n\tcust_fee_fix:'%s', cust_fee_var:'%s',\n\tmerch_fee_fix:'%s', merch_fee_var:'%s',\n\tmin:'%s', max:'%s'",
+                $feePlan['installments_count'],
+                $feePlan['allowed'] ? 'Yes' : 'No',
+                $feePlan['customer_fee_fixed'],
+                $feePlan['customer_fee_variable'],
+                $feePlan['merchant_fee_fixed'],
+                $feePlan['merchant_fee_variable'],
+                $feePlan['min_purchase_amount'],
+                $feePlan['max_purchase_amount']
+            );
+        }
+
+        return $plans;
     }
 }
