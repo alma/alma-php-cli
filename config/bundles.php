0e0a3470e23cb58b9936733457d217c8c5d6cfc7
Sylvain Just <sy.just@i-sloth.com>
2021-06-30 18:28:02 +0200

    feat: add logger support

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
