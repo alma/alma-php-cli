dace5dcd4fcf08fc2215808d76da1f0b3d31d354
Sylvain Just <sy.just@i-sloth.com>
2021-07-01 10:29:45 +0200

    feat: differents outputs for fee_plans from merchant or direct api

--- src/Command/AlmaMerchantGetCommand.php
+++ src/Command/AlmaMerchantGetCommand.php
@@ -2,18 +2,42 @@
 
 namespace App\Command;
 
+use Alma\API\Entities\FeePlan;
+use Alma\API\Entities\Merchant;
 use Alma\API\RequestError;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class AlmaMerchantGetCommand extends AbstractAlmaCommand
 {
+    const FEE_PLANS_TABLE_HEADERS = [
+        'cnt',
+        'allowed',
+        'cust_fee_fix',
+        'cust_fee_var',
+        'merch_fee_fix',
+        'merch_fee_var',
+        'min',
+        'max',
+    ];
+    const EXTENDED_DATA_API       = '/v1/me/extended-data';
+    const FEE_PLANS_API         = '/v1/me/fee-plans';
+    const DEFAULT_TABLE_HEADERS = ['Properties', 'Values'];
     protected static $defaultName = 'alma:merchant:get';
     protected static $defaultDescription = 'Get Merchant Informations';
+    private SymfonyStyle $io;
 
     protected function configure(): void
     {
+        $this->addOption(
+            'api-fee-plans-source',
+            'f',
+            InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
+            'Choose from which API endpoint you want get fee plans information',
+            [self::EXTENDED_DATA_API, self::FEE_PLANS_API]
+        );
     }
 
     /**
@@ -21,41 +45,12 @@ class AlmaMerchantGetCommand extends AbstractAlmaCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
+        $this->io = new SymfonyStyle($input, $output);
         $merchant = $this->alma->merchants->me();
-        $rows = [];
-        foreach (get_object_vars($merchant) as $property => $value) {
-            if (!in_array($property, ['fee_plans', 'legal_entity'])) {
-                $rows[] = [$property, $this->formatValue($value)];
-            }
-        }
+        $this->outputMerchant($merchant);
+        $this->outputLegalEntity($merchant);
 
-        $io->table(['Properties', 'Values'], $rows);
-        $fee_plans = [];
-        foreach ($merchant->fee_plans as $fee_plan) {
-
-            $fee_plans[] = [
-                $fee_plan['installments_count'],
-                $this->formatValue($fee_plan['allowed']),
-                $fee_plan['customer_fee_fixed'],
-                $fee_plan['customer_fee_variable'],
-                $fee_plan['merchant_fee_fixed'],
-                $fee_plan['merchant_fee_variable'],
-                $fee_plan['min_purchase_amount'],
-                $fee_plan['max_purchase_amount'],
-            ];
-        }
-        $fee_plans_headers = [
-            'cnt',
-            'allowed',
-            'cust_fee_fix',
-            'cust_fee_var',
-            'merch_fee_fix',
-            'merch_fee_var',
-            'min',
-            'max',
-        ];
-        $io->table($fee_plans_headers, $fee_plans);
+        $this->outputFeePlans($input->getOption('api-fee-plans-source'), $merchant);
 
 
         return self::SUCCESS;
@@ -75,4 +70,100 @@ class AlmaMerchantGetCommand extends AbstractAlmaCommand
 
         return $value;
     }
+
+    /**
+     * @param Merchant $merchant
+     */
+    protected function outputLegalEntity(Merchant $merchant): void
+    {
+        $this->io->title('Legal Entity');
+        $rows = [];
+        foreach ($merchant->legal_entity as $key => $value) {
+            if ($key !== 'address') {
+                $rows[] = [$key, $value];
+            }
+        }
+        $this->io->table(self::DEFAULT_TABLE_HEADERS, $rows);
+    }
+
+    /**
+     * @param Merchant $merchant
+     */
+    protected function outputMerchant(Merchant $merchant)
+    {
+        $this->io->title('Merchant');
+        $rows = [];
+        foreach (get_object_vars($merchant) as $property => $value) {
+            if (!in_array($property, ['fee_plans', 'legal_entity'])) {
+                $rows[] = [$property, $this->formatValue($value)];
+            }
+        }
+        $this->io->table(self::DEFAULT_TABLE_HEADERS, $rows);
+    }
+
+    /**
+     * @param array    $sources
+     * @param Merchant $merchant
+     *
+     * @throws RequestError
+     */
+    protected function outputFeePlans(array $sources, Merchant $merchant): void
+    {
+        foreach ($sources as $source) {
+            $fee_plans = [];
+            $this->io->title(sprintf('fee plans from %s', $source));
+            switch ($source) {
+                case self::FEE_PLANS_API:
+                    foreach ($merchant->fee_plans as $fee_plan) {
+                        $fee_plans[] = $this->populateRowFromArray($fee_plan);
+                    }
+                    break;
+                case self::EXTENDED_DATA_API:
+                    foreach ($this->alma->merchants->feePlans() as $feePlan) {
+                        $fee_plans[] = $this->populateRowFromObject($feePlan);
+                    }
+                    break;
+                default:
+            }
+            $this->io->table(self::FEE_PLANS_TABLE_HEADERS, $fee_plans);
+        }
+    }
+
+    /**
+     * @param array $fee_plan
+     *
+     * @return array
+     */
+    protected function populateRowFromArray(array $fee_plan): array
+    {
+        return [
+            $fee_plan['installments_count'],
+            $this->formatValue($fee_plan['allowed']),
+            $fee_plan['customer_fee_fixed'],
+            $fee_plan['customer_fee_variable'],
+            $fee_plan['merchant_fee_fixed'],
+            $fee_plan['merchant_fee_variable'],
+            $fee_plan['min_purchase_amount'],
+            $fee_plan['max_purchase_amount'],
+        ];
+}
+
+    /**
+     * @param FeePlan $feePlan
+     *
+     * @return array
+     */
+    protected function populateRowFromObject(FeePlan $feePlan): array
+    {
+        return [
+            $feePlan->installments_count,
+            $this->formatValue($feePlan->allowed),
+            $feePlan->customer_fee_fixed,
+            $feePlan->customer_fee_variable,
+            $feePlan->merchant_fee_fixed,
+            $feePlan->merchant_fee_variable,
+            $feePlan->min_purchase_amount,
+            $feePlan->max_purchase_amount,
+        ];
+}
 }
