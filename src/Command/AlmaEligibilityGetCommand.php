90ef6aadb28f472a05c68eee74419832a32fe89f
Sylvain Just <sy.just@i-sloth.com>
2021-07-09 19:24:24 +0200

    feat(wip): implement eligibility v2

--- src/Command/AlmaEligibilityGetCommand.php
+++ src/Command/AlmaEligibilityGetCommand.php
@@ -4,7 +4,7 @@ namespace App\Command;
 
 use Alma\API\Endpoints\Results\Eligibility;
 use Alma\API\RequestError;
-use DateTime;
+use Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,6 +13,35 @@ class AlmaEligibilityGetCommand extends AbstractAlmaCommand
 {
     protected static $defaultName = 'alma:eligibility:get';
     protected static $defaultDescription = 'Add a short description for your command';
+
+    /**
+     * @param $installment_definition
+     *
+     * @return array
+     * @throws Exception
+     */
+    protected function buildEligibilityQueryParams($installment_definition): array
+    {
+        $query = [];
+        if (preg_match("#([0-9]+)(-([0-9]+)([mMdD]))?$#", $installment_definition, $match)) {
+            $query['installments_count'] = intval($match[1]);
+            if (isset($match[4])) {
+                switch (strtoupper($match[4])) {
+                    case 'D':
+                        $query['deferred_days'] = intval($match[3]);
+                        break;
+                    case 'M':
+                        $query['deferred_months'] = intval($match[3]);
+                        break;
+                }
+            }
+        }
+        if (empty($query)) {
+            throw new Exception(sprintf('%s: bad installment definition', $installment_definition));
+        }
+
+        return $query;
+    }
 
     protected function configure()
     {
@@ -28,35 +57,38 @@ class AlmaEligibilityGetCommand extends AbstractAlmaCommand
                 'installments',
                 'i',
                 InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
-                'Define installments to check',
-                [2, 3, 4, 10]
+                "Define installments to check (formatted as INSTALLMENT[-DEFERRED]\nwhere INSTALLMENT is an integer\nand where DEFERRED is formatted as integer[MD] for Months or Days",
+                ["1-15d", 2, 3, 4, 10]
+            )
+            ->addOption(
+                'api-version',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Eligibility version to use',
+                1
             )
         ;
     }
 
+    /**
+     * @throws Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $amount        = $input->getOption('amount');
-        $installments        = $input->getOption('installments');
+        $amount       = $input->getOption('amount');
+        $installments = $input->getOption('installments');
+        $version      = intval($input->getOption('api-version'));
         try {
-            $eligibilities = $this->alma->payments->eligibility(
-                [
-                    'payment' => [
-                        'purchase_amount'    => $amount,
-                        'installments_count' => array_map(
-                            function ($installment) {
-                                return intval($installment);
-                            },
-                            $installments
-                        ),
-                    ],
-                ]
+            $eligibilityData = $this->getEligibilityData($amount, $installments, $version);
+            $this->outputFormat('json', $eligibilityData);
+            $eligibilities   = $this->alma->payments->eligibility(
+                $eligibilityData
             );
         } catch (RequestError $e) {
             return $this->outputRequestError($e);
         }
-        $headers       = ["Fee Count", "Eligible", "Plans", "Reason", "Min", "Max"];
-        $rows          = [];
+        $headers = ["Fee Count", "Eligible", "Plans", "Reason", "Min", "Max"];
+        $rows    = [];
         foreach ($eligibilities as $cnt => $eligibility) {
             $reasons        = $eligibility->getReasons();
             $constraints    = $eligibility->getConstraints();
@@ -74,7 +106,14 @@ class AlmaEligibilityGetCommand extends AbstractAlmaCommand
                 $this->formatMoney($maximum),
             ];
         }
-        $this->io->title(sprintf('Check Eligibility for %s on following installments [%s]', $this->formatMoney($amount), implode(', ', $installments)));
+        $this->io->title(
+            sprintf(
+                'Check Eligibility v%s for %s on following installments [%s]',
+                $version,
+                $this->formatMoney($amount),
+                implode(', ', $installments)
+            )
+        );
         $this->io->table($headers, $rows);
 
         return self::SUCCESS;
@@ -108,4 +147,40 @@ class AlmaEligibilityGetCommand extends AbstractAlmaCommand
         return $plans;
     }
 
+    /**
+     * @param int   $amount
+     * @param array $installments
+     * @param int   $version
+     *
+     * @return array[]
+     * @throws Exception
+     */
+    protected function getEligibilityData(int $amount, array $installments, int $version): array
+    {
+        switch ($version) {
+            case 1:
+                return [
+                    'payment' => [
+                        'purchase_amount'    => $amount,
+                        'installments_count' => array_map(
+                            function ($installment) {
+                                return intval($installment);
+                            },
+                            $installments
+                        ),
+                    ],
+                ];
+            case 2:
+                $queries = [];
+                foreach ($installments as $installment_definition) {
+                    $queries[] = $this->buildEligibilityQueryParams($installment_definition);
+                }
+
+                return ['purchase_amount' => $amount, 'queries' => $queries];
+
+
+            default:
+                throw new Exception(sprintf('version %s : WTF !!!', $version));
+        }
+    }
 }
