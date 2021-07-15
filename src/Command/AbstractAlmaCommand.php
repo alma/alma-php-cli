0b7753eb2a4c4302c716a641899045cd0e95deb0
Sylvain Just <sy.just@i-sloth.com>
2021-07-15 10:20:17 +0200

    feat: add force-live-env command security

--- src/Command/AbstractAlmaCommand.php
+++ src/Command/AbstractAlmaCommand.php
@@ -8,6 +8,7 @@ use Alma\API\RequestError;
 use DateTime;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -15,12 +16,23 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  * Class AbstractAlmaCommand
  *
  * @package App\Command
+ * @TODO split this abstract in 2 read & write classes
  */
 abstract class AbstractAlmaCommand extends Command
 {
     public const DEFAULT_TABLE_HEADERS = ['Properties', 'Values'];
     protected Client $alma;
     protected SymfonyStyle $io;
+
+    /**
+     * AbstractAlmaCommand constructor.
+     */
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+        #TODO: pull down this process in an abstract write sub class
+        $this->addOption('force-live-env', null, InputOption::VALUE_NONE, 'force execution without interaction if live key is given in ALMA_API_KEY environment');
+    }
 
     /**
      * @param RequestError $exception
@@ -198,6 +210,15 @@ abstract class AbstractAlmaCommand extends Command
     public function run(InputInterface $input, OutputInterface $output): int
     {
         $this->io = new SymfonyStyle($input, $output);
+        #TODO: pull down this process in an abstract write sub class
+        if (preg_match("#sk_live#", $_ENV['ALMA_API_KEY']) && !$input->getOption('force-live-env')) {
+            $this->io->warning("Your ALMA_API_KEY seems to be a LIVE environment API KEY");
+            $response = $this->io->choice(sprintf("Are you sure you want perform '%s' in LIVE environment ?", $this->getName()), ['No', 'Yes'], 'No');
+            if ($response === 'No') {
+                $this->io->info(sprintf('command "%s" halted by user', $this->getName()));
+                return self::SUCCESS;
+            }
+        }
         return parent::run($input, $output);
     }
 
