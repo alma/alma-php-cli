a7078f5c4b32573eb7fcc47b17b24bd9805e06db
Sylvain Just <sy.just@i-sloth.com>
2021-07-27 14:57:14 +0200

    feat: add min & max options (from & to) into accounting-transaction:get command

--- src/Command/AlmaAccountingTransactionsGetCommand.php
+++ src/Command/AlmaAccountingTransactionsGetCommand.php
@@ -4,8 +4,11 @@ namespace App\Command;
 
 use Alma\API\RequestError;
 use App\Endpoints\AlmaBase;
+use DateTime;
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AlmaAccountingTransactionsGetCommand extends AbstractAlmaCommand
@@ -15,14 +18,34 @@ class AlmaAccountingTransactionsGetCommand extends AbstractAlmaCommand
 
     /**
      * @throws RequestError
+     * @throws Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $endpoint = new AlmaBase($this->almaClient->getContext());
         $this->io->title('/v1/accounting/transactions');
-        foreach ($endpoint->request('/v1/accounting/transactions')->get()->json['data'] as $key => $datum) {
+        $request = $endpoint->request('/v1/accounting/transactions');
+        $queryParams = [];
+        if ($from = $input->getOption('from')) {
+            $queryParams['created[min]'] = (new DateTime($from))->getTimestamp();
+        }
+        if ($to = $input->getOption('to')) {
+            $queryParams['created[max]'] = (new DateTime($to))->getTimestamp();
+        }
+        if ($queryParams) {
+            $request->setQueryParams($queryParams);
+        }
+        foreach ($request->get()->json['data'] as $key => $datum) {
             $this->outputKeyValueTable(array_merge(['key' => $key], $datum));
         }
         return Command::SUCCESS;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addOption('from', 'f', InputOption::VALUE_REQUIRED, 'from date as YYYY-MM-DD [HH:mm:ss]')
+            ->addOption('to', 't', InputOption::VALUE_REQUIRED, 'to date as YYYY-MM-DD [HH:mm:ss]')
+        ;
     }
 }
